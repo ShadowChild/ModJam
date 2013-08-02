77b9352afcb6d41c7f38f542bd9155a9a2d76e9e
Darkhax <darklime@live.ca>
2013-08-01 23:48:42 -0600

    Slowly but surely

--- src/shadowhax/modjam/item/ItemLinkingCrystal.java
+++ src/shadowhax/modjam/item/ItemLinkingCrystal.java
@@ -2,8 +2,13 @@ package shadowhax.modjam.item;
 
 import java.util.List;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 import shadowhax.modjam.ModJam;
 import shadowhax.modjam.block.Blocks;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -18,9 +23,48 @@ public class ItemLinkingCrystal extends Item{
 		this.hasSubtypes = true;
 	}
 	
-    public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int par7, float par8, float par9, float par10) {   	
+	private int posX;
+	private int posY;
+	private int posZ;
+	
+    public void onUpdate(ItemStack stack, World world, Entity entity, int par4, boolean par5) {
+    	
     	stack.setTagCompound(new NBTTagCompound());
+    	posX = stack.stackTagCompound.getInteger("linkX");
+    	posY = stack.stackTagCompound.getInteger("linkY");
+    	posZ = stack.stackTagCompound.getInteger("linkZ");
+    }
+	
+    public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int par7, float par8, float par9, float par10) {   	
 
+    	if (stack.getItemDamage() == 0){        	
+        	if (world.getBlockId(x, y, z) == Blocks.warpBlock.blockID) {
+            	stack.stackTagCompound.setInteger("linkX", x);
+            	stack.stackTagCompound.setInteger("linkY", y);
+            	stack.stackTagCompound.setInteger("linkZ", z);
+            	
+            	stack.setItemDamage(1);
+        	}
+    	}
+    	
+    	if (stack.getItemDamage() == 1) {
+    		if(world.getBlockId(posX, posY, posZ) == Blocks.warpBlock.blockID){
+    			player.posX = this.posX;
+    			player.posY = this.posY;
+    			player.posZ = this.posZ;
+    			player.setPositionAndUpdate(posX, posY, posZ);
+    			--stack.stackSize;
+    		}
+    	}
         return true;
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public void getSubItems(int par1, CreativeTabs par2CreativeTabs, List par3List)
+    {
+        for (int j = 0; j < 2; ++j)
+        {
+            par3List.add(new ItemStack(par1, 1, j));
+        }
     }
 }
