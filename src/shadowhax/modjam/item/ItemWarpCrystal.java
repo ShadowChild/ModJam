a76b93d6f04e400b7fb43e8dea13d6580be6f9ee
Darkhax <darklime@live.ca>
2013-08-01 21:04:04 -0600

    Failing hard with pos

--- src/shadowhax/modjam/item/ItemWarpCrystal.java
+++ src/shadowhax/modjam/item/ItemWarpCrystal.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import shadowhax.modjam.ModJam;
 import shadowhax.modjam.block.Blocks;
+import shadowhax.modjam.core.helpers.RayTraceHelper;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumToolMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
 public class ItemWarpCrystal extends Item {
@@ -20,21 +22,21 @@ public class ItemWarpCrystal extends Item {
 		this.setCreativeTab(ModJam.tab);
 	}
 	
-	public static int warpX;
-	public static int warpY;
-	public static int warpZ;
-	
-	public static int block;
+	public static double warpX;
+	public static double warpY;
+	public static double warpZ;
 	
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int par7, float par8, float par9, float par10) {
         if (world.getBlockId(x, y, z) == Blocks.warpBlock.blockID) {
-        	stack.stackTagCompound.setInteger("linkX", x);
-        	stack.stackTagCompound.setInteger("linkY", y);
-        	stack.stackTagCompound.setInteger("linkZ", z);
+        	
+        	stack.stackTagCompound.setDouble("linkX", player.posX);
+        	stack.stackTagCompound.setDouble("linkY", player.posY);
+        	stack.stackTagCompound.setDouble("linkZ", player.posZ);
         	
         	warpX = stack.stackTagCompound.getInteger("linkX");
         	warpY = stack.stackTagCompound.getInteger("linkY");
         	warpZ = stack.stackTagCompound.getInteger("linkZ");
+        	}
         }
         
         if (world.getBlockId(x, y, z) != Blocks.warpBlock.blockID && stack.stackTagCompound.hasKey("linkX")){
