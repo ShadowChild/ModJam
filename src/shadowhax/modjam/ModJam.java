046077ce5cef10ad7409f96e818791a6413e4b29
ShadowChild <shadychild12@gmail.com>
2013-08-02 01:05:36 +0100

    Initial Commit
    Signed-off-by: ShadowChild <shadychild12@gmail.com>

---
+++ src/shadowhax/modjam/ModJam.java
@@ -0,0 +1,34 @@
+package shadowhax.modjam;
+
+import shadowhax.modjam.util.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+public class ModJam {
+
+	@Mod.Instance(Reference.MOD_ID)
+	public static ModJam instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent evt) {
+		
+		
+	}
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent evt) {
+		
+		
+	}
+	
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent evt) {
+		
+		
+	}
+}
---
+++ src/shadowhax/modjam/util/Reference.java
@@ -0,0 +1,8 @@
+package shadowhax.modjam.util;
+
+public class Reference {
+
+	public static final String MOD_ID = "ShadowHax_ModJam"; // TODO: Change Later
+	public static final String MOD_NAME = "ShadowHax_ModJam"; // TODO: Change Later
+	public static final String MOD_VERSION = "v1 indev";
+}
