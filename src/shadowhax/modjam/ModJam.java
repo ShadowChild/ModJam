8b70006b3e5e3f3573f57f446b6f5b5af7e9cdbe
Darkhax <darklime@live.ca>
2013-08-01 18:50:12 -0600

    Finished setting up proxy

--- src/shadowhax/modjam/ModJam.java
+++ src/shadowhax/modjam/ModJam.java
@@ -1,8 +1,10 @@
 package shadowhax.modjam;
 
 import net.minecraft.creativetab.CreativeTabs;
+import shadowhax.modjam.core.proxy.CommonProxy;
 import shadowhax.modjam.util.Reference;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -11,6 +13,9 @@ import cpw.mods.fml.common.network.NetworkMod;
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class ModJam {
+	
+	@SidedProxy(serverSide= Reference.SERVER_PROXY, clientSide= Reference.CLIENT_PROXY)
+	public static CommonProxy proxy;
 
 	@Mod.Instance(Reference.MOD_ID)
 	public static ModJam instance;
@@ -26,13 +31,11 @@ public class ModJam {
 	
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent evt) {
-		
-		
+			
 	}
 	
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent evt) {
-		
 		
 	}
 }
