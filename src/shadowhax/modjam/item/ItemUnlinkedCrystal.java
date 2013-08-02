b0ac8ba1919ac5ba2348b49c83e7c1dd9cec8e5d
Darkhax <darklime@live.ca>
2013-08-01 23:20:32 -0600

    Back, Learned a wonderful thing about nbt from jake. FFS >>

--- src/shadowhax/modjam/item/ItemUnlinkedCrystal.java
+++ src/shadowhax/modjam/item/ItemUnlinkedCrystal.java
@@ -4,6 +4,7 @@ import shadowhax.modjam.ModJam;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
 public class ItemUnlinkedCrystal extends Item{
@@ -15,6 +16,7 @@ public class ItemUnlinkedCrystal extends Item{
 	
     public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
     {
+    	par1ItemStack.setTagCompound(new NBTTagCompound());
         par1ItemStack.stackTagCompound.setInteger("ffs", 666);
         return true;
     }
