e3e71243cc9e3a35c4dd68745d4b69639341dc88
Darkhax <darklime@live.ca>
2013-08-01 19:31:22 -0600

    Working on new warp crystal

---
+++ src/shadowhax/modjam/block/BlockWarpCrystal.java
@@ -0,0 +1,14 @@
+package shadowhax.modjam.block;
+
+import shadowhax.modjam.ModJam;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockWarpCrystal extends Block
+{
+    public BlockWarpCrystal(int par1) {
+    	
+        super(par1, Material.glass);
+        this.setCreativeTab(ModJam.tab);
+    }
+}
--- src/shadowhax/modjam/block/Blocks.java
+++ src/shadowhax/modjam/block/Blocks.java
@@ -1,10 +1,15 @@
 package shadowhax.modjam.block;
 
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.minecraft.block.Block;
+
 public class Blocks {
 	
-	public Blocks() {
+	public static final Block WarpTest = new BlockWarpCrystal(1337).setUnlocalizedName("block.warcrystal.name");
+	
+	public static void addNames() {
 		
-		
+		LanguageRegistry.addName(WarpTest, "Warp Crystal Test");
 	}
 
 }
