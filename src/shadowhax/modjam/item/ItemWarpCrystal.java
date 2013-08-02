4da38c8c9a202f5a9fbc602ce7a3b3ceebd7b63e
Darkhax <darklime@live.ca>
2013-08-01 20:32:39 -0600

    Created random nbt teleport code

--- src/shadowhax/modjam/item/ItemWarpCrystal.java
+++ src/shadowhax/modjam/item/ItemWarpCrystal.java
@@ -1,6 +1,9 @@
 package shadowhax.modjam.item;
 
+import java.util.List;
+
 import shadowhax.modjam.ModJam;
+import shadowhax.modjam.block.Blocks;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumToolMaterial;
@@ -17,12 +20,37 @@ public class ItemWarpCrystal extends Item {
 		this.setCreativeTab(ModJam.tab);
 	}
 	
-    public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int par7, float par8, float par9, float par10)
-    {
-        if world.getBlockId(x, y, z){
+	public static int warpX;
+	public static int warpY;
+	public static int warpZ;
+	
+    public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int par7, float par8, float par9, float par10) {
+        if (world.getBlockId(x, y, z) == Blocks.warpBlock.blockID) {
         	
+        	stack.stackTagCompound.setInteger("linkX", x);
+        	stack.stackTagCompound.setInteger("linkY", y);
+        	stack.stackTagCompound.setInteger("linkZ", y);
+        	
+        	warpX = stack.stackTagCompound.getInteger("linkX");
+        	warpY = stack.stackTagCompound.getInteger("linkY");
+        	warpZ = stack.stackTagCompound.getInteger("linkZ");
         }
         
+        if (world.getBlockId(x, y, z) != Blocks.warpBlock.blockID && stack.stackTagCompound.hasKey("linkX")){
+        	if (world.getBlockId(warpY, warpY, warpZ) != 0 && world.getBlockId(warpY, warpY, warpZ) == Blocks.warpBlock.blockID ){
+        		
+        		player.posX = warpX;
+        		player.posY = warpY;
+        		player.posZ = warpZ;
+        		player.setPositionAndUpdate(warpX, warpY, warpZ);
+        		--stack.stackSize;
+        	}	
+        }  
+		return true;    
+    }	
+    
+    public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par4) {
+    	
+    	list.add("X"+ warpX + " Y"+ warpY + " Z" + warpZ);
     }
-	
 }
