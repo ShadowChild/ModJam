630422af8f6cd72add84290509345d68c1f8fb65
Darkhax <darklime@live.ca>
2013-08-01 20:43:21 -0600

    Created RayTraceHelper.

--- src/shadowhax/modjam/item/ItemWarpCrystal.java
+++ src/shadowhax/modjam/item/ItemWarpCrystal.java
@@ -24,12 +24,14 @@ public class ItemWarpCrystal extends Item {
 	public static int warpY;
 	public static int warpZ;
 	
+	public static int block;
+	
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int par7, float par8, float par9, float par10) {
         if (world.getBlockId(x, y, z) == Blocks.warpBlock.blockID) {
         	
         	stack.stackTagCompound.setInteger("linkX", x);
         	stack.stackTagCompound.setInteger("linkY", y);
-        	stack.stackTagCompound.setInteger("linkZ", y);
+        	stack.stackTagCompound.setInteger("linkZ", z);
         	
         	warpX = stack.stackTagCompound.getInteger("linkX");
         	warpY = stack.stackTagCompound.getInteger("linkY");
