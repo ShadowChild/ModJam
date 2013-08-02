79b6dc2484f4fa278880e2bcf02cd24a761a0b66
Darkhax <darklime@live.ca>
2013-08-01 21:19:37 -0600

    Working on event

---
+++ src/shadowhax/modjam/core/events/InteractionHandler.java
@@ -0,0 +1,26 @@
+package shadowhax.modjam.core.events;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumMovingObjectType;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
+
+public class InteractionHandler {
+	
+	@ForgeSubscribe
+	public void interactEvent(PlayerInteractEvent event){
+		
+		if (event.useItem != null){
+		}
+	}
+}
--- src/shadowhax/modjam/item/ItemWarpCrystal.java
+++ src/shadowhax/modjam/item/ItemWarpCrystal.java
@@ -5,6 +5,7 @@ import java.util.List;
 import shadowhax.modjam.ModJam;
 import shadowhax.modjam.block.Blocks;
 import shadowhax.modjam.core.helpers.RayTraceHelper;
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumToolMaterial;
@@ -25,32 +26,6 @@ public class ItemWarpCrystal extends Item {
 	public static double warpX;
 	public static double warpY;
 	public static double warpZ;
-	
-    public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int par7, float par8, float par9, float par10) {
-        if (world.getBlockId(x, y, z) == Blocks.warpBlock.blockID) {
-        	
-        	stack.stackTagCompound.setDouble("linkX", player.posX);
-        	stack.stackTagCompound.setDouble("linkY", player.posY);
-        	stack.stackTagCompound.setDouble("linkZ", player.posZ);
-        	
-        	warpX = stack.stackTagCompound.getInteger("linkX");
-        	warpY = stack.stackTagCompound.getInteger("linkY");
-        	warpZ = stack.stackTagCompound.getInteger("linkZ");
-        	}
-        }
-        
-        if (world.getBlockId(x, y, z) != Blocks.warpBlock.blockID && stack.stackTagCompound.hasKey("linkX")){
-        	if (world.getBlockId(warpY, warpY, warpZ) != 0 && world.getBlockId(warpY, warpY, warpZ) == Blocks.warpBlock.blockID ){
-        		
-        		player.posX = warpX;
-        		player.posY = warpY;
-        		player.posZ = warpZ;
-        		player.setPositionAndUpdate(warpX, warpY, warpZ);
-        		--stack.stackSize;
-        	}	
-        }  
-		return true;    
-    }	
     
     public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par4) {
     	
