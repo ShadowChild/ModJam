427a1c8fc54b63b1fd42a725ff46bc3bffb1d170
Darkhax <darklime@live.ca>
2013-08-02 18:00:19 -0600

    Created block. Really need that gui to be set up >>

--- src/shadowhax/modjam/block/BlockRefiningTable.java
+++ src/shadowhax/modjam/block/BlockRefiningTable.java
@@ -1,5 +1,30 @@
 package shadowhax.modjam.block;
 
-public class BlockRefiningTable {
+import shadowhax.modjam.ModJam;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
 
+public class BlockRefiningTable extends Block {
+	
+	public BlockRefiningTable(int i) {
+		
+	    super(i, Material.glass);
+	    this.setCreativeTab(ModJam.tab);
+	}
+
+	public boolean onBlockActivated(World var1, int var2, int var3, int var4, EntityPlayer player, int var6, float var7, float var8, float var9) {
+		
+		if (!player.isSneaking()) {
+			
+			player.openGui(ModJam.instance, 7, var1, var2, var3, var4);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
