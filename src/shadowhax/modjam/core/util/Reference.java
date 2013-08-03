fa6f61a5820f7b186bd932067070d01b66bc3098
Darkhax <darklime@live.ca>
2013-08-03 04:44:40 -0600

    Added a logger.

--- src/shadowhax/modjam/core/util/Reference.java
+++ src/shadowhax/modjam/core/util/Reference.java
@@ -1,5 +1,9 @@
 package shadowhax.modjam.core.util;
 
+import java.util.logging.Logger;
+
+import cpw.mods.fml.common.FMLLog;
+
 public class Reference {
 
 	public static final String MOD_ID = "ShadowHax_ModJam"; // TODO: Change Later
@@ -8,4 +12,10 @@ public class Reference {
 	
 	public static final String CLIENT_PROXY = "shadowhax.modjam.core.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "shadowhax.modjam.core.proxy.CommonProxy";
+    
+	public static Logger logger = Logger.getLogger("ShadowHax");
+	
+	static {
+		logger.setParent(FMLLog.getLogger());
+	}
 }
