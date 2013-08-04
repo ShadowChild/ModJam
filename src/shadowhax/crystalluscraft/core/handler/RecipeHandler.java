b0eb9a98415d68c098679ace5b39ad69cbc0a5d0
Darkhax <darklime@live.ca>
2013-08-04 00:19:17 -0600

    Made recipe handler -Recommit, no changes

---
+++ src/shadowhax/crystalluscraft/core/handler/RecipeHandler.java
@@ -0,0 +1,29 @@
+package shadowhax.crystalluscraft.core.handler;
+
+public class RecipeHandler {
+	
+	public static void registerRecipes(){
+		
+		registerVanillaShapedRecipes();
+		registerVanillaShapelessRecipes();
+		registerRefinedShapedRecipes();
+		registerRefinedShapelessRecipes();
+	}
+	
+	public static void registerVanillaShapedRecipes() {
+		
+	}
+	
+	public static void registerVanillaShapelessRecipes() {
+		
+	}
+	
+	public static void registerRefinedShapedRecipes() {
+		
+	}
+	
+	public static void registerRefinedShapelessRecipes() {
+		
+	}
+
+}
