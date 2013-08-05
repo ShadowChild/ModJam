d9c57c90d21d1bc0a5fbf959dac0ca916a87afa6
Darkhax <darklime@live.ca>
2013-08-04 22:26:28 -0600

    set up textures for workbench

--- src/shadowhax/crystalluscraft/block/BlockRefiningTable.java
+++ src/shadowhax/crystalluscraft/block/BlockRefiningTable.java
@@ -1,20 +1,41 @@
 package shadowhax.crystalluscraft.block;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import shadowhax.crystalluscraft.CrystallusCraft;
 import shadowhax.crystalluscraft.core.util.Config;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 
 public class BlockRefiningTable extends Block {
 	
+	private Icon iconSide;
+	private Icon iconTop;
+	private Icon iconFront;
+	private Icon iconBottom;
+
 	public BlockRefiningTable(int i) {
 		
 	    super(i, Material.glass);
 	    this.setCreativeTab(CrystallusCraft.tab);
 	}
 
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister par1IconRegister) {
+        this.iconSide = par1IconRegister.registerIcon("crystalluscraft:Crystal Crafting Side");
+        this.iconTop = par1IconRegister.registerIcon("crystalluscraft:Crystal Crafting Top");
+        this.iconBottom = par1IconRegister.registerIcon("crystalluscraft:Crystal Crafting Bottom");
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public Icon getIcon(int par1, int par2) {
+        return par1 == 1 ? this.iconTop : (par1 == 0 ?this.iconBottom : (par1 != 2 && par1 != 4 ? this.iconSide : this.iconSide));
+    }
+    
 	public boolean onBlockActivated(World var1, int var2, int var3, int var4, EntityPlayer player, int var6, float var7, float var8, float var9) {
 		
 		if (!player.isSneaking()) {
