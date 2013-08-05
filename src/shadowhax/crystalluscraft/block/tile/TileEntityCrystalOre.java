ee3b198b1befcbd989dc521273249e993dbe5c2e
ShadowChild <shadychild12@gmail.com>
2013-08-05 23:34:28 +0100

    Remove un-neccessary nbt savings
    Signed-off-by: ShadowChild <shadychild12@gmail.com>

--- src/shadowhax/crystalluscraft/block/tile/TileEntityCrystalOre.java
+++ src/shadowhax/crystalluscraft/block/tile/TileEntityCrystalOre.java
@@ -1,6 +1,5 @@
 package shadowhax.crystalluscraft.block.tile;
 
-import net.minecraft.block.Block;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 
@@ -10,17 +9,11 @@ public class TileEntityCrystalOre extends TileEntity {
     public void readFromNBT(NBTTagCompound nbt) {
 
         super.readFromNBT(nbt);
-
-        this.blockType = Block.blocksList[nbt.getInteger("BlockId")];
-        this.blockMetadata = nbt.getInteger("BlockMetadata");
     }
 
     @Override
     public void writeToNBT(NBTTagCompound nbt) {
 
         super.writeToNBT(nbt);
-
-        nbt.setInteger("BlockId", this.blockType.blockID);
-        nbt.setInteger("BlockMetadata", this.blockMetadata);
     }
 }
